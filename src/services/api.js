4cdd4a282bd7634aeee1ac29eef258bab0c982fe
VEDULAPURAM SAI PHANEENDRA KUMAR <97967032+Saiphanikum@users.noreply.github.com>
2025-09-09 20:05:16 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "e9d0b5dc5bd264f3bd0ec81aaa672ee1";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
@@ -9,9 +9,7 @@ export const getPopularMovies = async () => {
 
 export const searchMovies = async (query) => {
   const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
-      query
-    )}`
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
   );
   const data = await response.json();
   return data.results;
